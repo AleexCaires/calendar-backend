5766a2259548b57c10e4a76186e0ab3640c53349
AleexCaires <tiagoaleex98@gmail.com>
2024-05-31 10:32:39 +0100

    Changes to project

--- my-vercel-backend/index.mjs
+++ my-vercel-backend/index.mjs
@@ -1,8 +1,9 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
 });
 
 const port = process.env.PORT || 3000;
