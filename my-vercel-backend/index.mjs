9546be0ca7876e40687aeb8ad0bbfdf0fd4a46e5
AleexCaires <tiagoaleex98@gmail.com>
2024-05-31 10:35:39 +0100

    new Changes

--- my-vercel-backend/index.mjs
+++ my-vercel-backend/index.mjs
@@ -3,8 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+  res.status(200).send('Hello, world new version!');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+ 
